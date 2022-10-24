74180389aa60d0a951fc7cd20a06d7135e0a6238
developermithu <www.mithudas77@gmail.com>
2022-10-25 00:55:49 +0600

    migrate vite to mix with breeze auth

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script src="{{ mix('js/app.js') }}" defer></script>
+</head>
+
+<body>
+
+    <h1 class="text-4xl text-center font-bold text-blue-500 bg-gray-900">Welcome to webpack mix again</h1>
+
+</body>
+
+</html>
